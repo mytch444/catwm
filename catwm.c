581cfc3f281b354330b4b2b6eee29defca0191bc
mytchel <mytchel.hammond@gmail.com>
2014-01-14 13:04:26 +1300

    fixed showmessage bug

--- catwm.c
+++ catwm.c
@@ -914,15 +914,15 @@ void message_full(char* args[], int argc) {
   char* command[argc + 4];
   command[0] = "showmessage";
   command[1] = "-x";
-  command[2] = malloc(sizeof(char) * 10);
+  command[2] = malloc(sizeof(char) * 20);
   sprintf(command[2], "%i", sx + sw - 20);
   
-  for (i = 0; args[i]; i++) {
+  for (i = 0; i < argc; i++) {
     command[i + 3] = args[i];
   }
 
   command[argc + 3] = NULL;
-  
+
   struct Arg arg;
   arg.com = command;
   spawn(arg);
@@ -940,7 +940,7 @@ void message_wait(char *message, int t) {
   char* command[] = {NULL, "-t", NULL, "-w"};
 
   command[0] = message;
-  command[2] = malloc(sizeof(char) * 5);
+  command[2] = malloc(sizeof(char) * 20);
   sprintf(command[2], "%i", t);
   
   message_full(command, 4);
@@ -994,7 +994,7 @@ void submap(struct Arg arg) {
   
   message("No key shortcut maped to that key");
   
- end:
+p end:
   releasekeyboard();
 }
 
